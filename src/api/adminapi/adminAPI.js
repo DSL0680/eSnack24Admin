2106b929f6451c6beeb3e5c3bf6017dcbd0ca674
jhyeong <hyeongcode@gmail.com>
2024-11-28 13:06:38 +0900

    feat: admin refreshRequest add

--- src/api/adminapi/adminAPI.js
+++ src/api/adminapi/adminAPI.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const host = 'http://localhost:8080/admin/api/v1/admin';
 
+export const refreshRequest = async (accessToken, refreshToken) => {
+
+    await axios.get(`${host}/refresh?refreshToken=${refreshToken}`, {
+        headers: {'Authorization': `Bearer ${accessToken}`}
+    })
+}
+
 export const getAllAdminList = async (page) => {
 
     const pageValue = (Number)(page || 1)
