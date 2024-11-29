3e616893af048b976763428a9e4da2ce0f50d3af
jhyeong <hyeongcode@gmail.com>
2024-11-29 17:56:58 +0900

    clore: sink

---
+++ src/pages/adminpages/AdminListPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminListComponent from "../../components/admincomponents/AdminListComponent.jsx";
+
+function AdminListPage() {
+    return (
+        <AdminListComponent/>
+    );
+}
+
+export default AdminListPage;
