65964734fab2819b7ace813664d8de19f719091b
DSL0680 <ekthf988172@gmail.com>
2024-11-30 16:57:37 +0900

    feat: graph component / page/ router add

---
+++ src/components/graphcomponents/GraphComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GraphComponent(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default GraphComponent;
---
+++ src/pages/graphpages/user/GraphPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function GraphPage(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default GraphPage;
