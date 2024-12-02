7913f7f14c2609432ef33ac3c250b700eadf2031
yun9699 <cha23215@daum.net>
2024-12-02 13:19:49 +0900

    feat: product detail function add

--- src/pages/productpages/ProductDetailPage.jsx
+++ src/pages/productpages/ProductDetailPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ProductDetailComponent from "../../components/productcomponents/ProductDetailComponent";
 
 function ProductDetailPage() {
     return (
-        <div></div>
+        <div>
+            <ProductDetailComponent />
+        </div>
     );
 }
 
