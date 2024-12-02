e882c398b755d05765c45609f71df0389a2eac98
LeeDongEon <eon@LeeDongEonui-MacBookPro.local>
2024-12-02 09:20:22 +0900

    feat: faq / qna - end

--- src/api/csapi/qnaAPI.js
+++ src/api/csapi/qnaAPI.js
@@ -9,7 +9,26 @@ export const getQNAList = async (page) => {
 
     const res = await jwtAxios.get(`${host}/list?page=${pageValue}`);
 
+    console.log("------")
     console.log(res.data);
+
+    return res.data;
+}
+
+export const getTQNAList = async (page) => { // status = true
+
+    const pageValue = (Number)(page || 1)
+
+    const res = await jwtAxios.get(`${host}/statuslist/true?page=${pageValue}`);
+
+    return res.data;
+}
+
+export const getFQNAList = async (page) => { // status = false
+
+    const pageValue = (Number)(page || 1)
+
+    const res = await jwtAxios.get(`${host}/statuslist/false?page=${pageValue}`);
 
     return res.data;
 }
