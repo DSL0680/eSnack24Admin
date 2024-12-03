c1d06490eeec83495a69199115921e351214533b
LeeDongEon <eon@LeeDongEonui-MacBookPro.local>
2024-12-03 15:28:14 +0900

    chore: qna inner product delete

--- src/pages/qnapages/QNAListPage.jsx
+++ src/pages/qnapages/QNAListPage.jsx
@@ -1,11 +1,11 @@
 import QNAListComponent from "../../components/qnacomponent/QNAListComponent.jsx";
 
 export const QNAListTableHeader = [
-    "제목", "상품명", "문의자", "담당자", "등록시간" ,"답변여부"
+    "제목", "문의자", "담당자", "등록시간" ,"답변여부"
 ]
 
 export const QNAListTableColumn = [
-    "qno", "qtitle", "ptitle_ko", "uemail", "admname", "gregdate", "qstatus"
+    "qno", "qtitle", "uemail", "admname", "gregdate", "qstatus"
 ]
 
 function QnaListPage() {
